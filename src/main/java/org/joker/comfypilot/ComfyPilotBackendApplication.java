1b4fc964179f48eb3688ffc41c197d7fdd6ea9c2
RuinedJoker <616401600@qq.com>
2026-01-14 10:37:47 +0800

    first commit

---
+++ src/main/java/org/joker/comfypilot/ComfyPilotBackendApplication.java
@@ -0,0 +1,13 @@
+package org.joker.comfypilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComfyPilotBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComfyPilotBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/joker/comfypilot/ComfyPilotBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.joker.comfypilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComfyPilotBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
