869356bbc64835ba5e60484b68bd9ff3f1186b22
RuinedJoker <616401600@qq.com>
2026-01-19 14:11:07 +0800

    feat:开启定时任务

--- src/main/java/org/joker/comfypilot/common/config/AsyncConfig.java
+++ src/main/java/org/joker/comfypilot/common/config/AsyncConfig.java
@@ -2,11 +2,13 @@ package org.joker.comfypilot.common.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * 异步配置
  */
 @Configuration
 @EnableAsync
+@EnableScheduling
 public class AsyncConfig {
 }
