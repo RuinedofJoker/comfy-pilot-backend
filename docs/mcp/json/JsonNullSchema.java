3bdd1f2ce62c321b77301f30be5144226381a86a
RuinedJoker <616401600@qq.com>
2026-01-25 11:20:55 +0800

    feat:删除多余的第三方依赖代码

--- docs/mcp/json/JsonNullSchema.java
+++
@@ -1,9 +0,0 @@
-package dev.langchain4j.model.chat.request.json;
-
-public class JsonNullSchema implements JsonSchemaElement {
-
-    @Override
-    public String description() {
-        return null;
-    }
-}
